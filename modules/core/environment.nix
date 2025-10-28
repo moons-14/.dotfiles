54ee77de4df8fc60e77787409894f4773dec7570
moons14 <moons@moons14.com>
2025-10-29 02:49:02 +0900

    fix: error

--- modules/core/environment.nix
+++ modules/core/environment.nix
@@ -3,5 +3,6 @@
   environment.sessionVariables = {
     ELECTRON_OZONE_PLATFORM_HINT = "wayland";
     QT_QPA_PLATFORM = "wayland;xcb";
+    NIXOS_OZONE_WL = "1";
   };
 }
--- profiles/gui.nix
+++ profiles/gui.nix
@@ -5,10 +5,10 @@
     ./../modules/core/niri.nix
     ./../modules/core/hyprland.nix
     ./../modules/core/greetd.nix
-    ./../modules/core/browser.nix
   ];
 
   home-manager.users.moons.imports = [
     ./../modules/home/vscode.nix
+    ./../modules/core/browser.nix
   ];
 }
