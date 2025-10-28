d5e51d7714443a6328c833ffef4f3c1b25be8fb1
moons14 <moons@moons14.com>
2025-10-29 02:46:23 +0900

    add browser

--- profiles/gui.nix
+++ profiles/gui.nix
@@ -5,6 +5,7 @@
     ./../modules/core/niri.nix
     ./../modules/core/hyprland.nix
     ./../modules/core/greetd.nix
+    ./../modules/core/browser.nix
   ];
 
   home-manager.users.moons.imports = [
