ea8a2556cc0045caa829f05d7f21331a84853b22
moons-14 <moons@moons14.com>
2025-10-29 18:19:28 +0900

    fix bugs

--- hosts/x1g9/hardware-configuration.nix
+++ hosts/x1g9/hardware-configuration.nix
@@ -8,18 +8,18 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/41636b9d-4d0d-4f4f-9f24-946041653d49";
+    { device = "/dev/disk/by-uuid/16b29578-6836-414b-a5e1-863bc21c5fc3";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/DE18-74DC";
+    { device = "/dev/disk/by-uuid/209A-C8C9";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
