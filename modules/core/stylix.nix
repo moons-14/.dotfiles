5486e564082e4a4acbab75641075bd890dfc7c3e
moons14 <moons@moons14.com>
2025-10-29 21:44:15 +0900

    fix error

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -2,6 +2,13 @@
 {
   stylix = {
     enable = true;
+
+    stylix.targets.gnome.enable = lib.mkForce false;
+    stylix.targets.gnomeShell.enable = lib.mkForce false;
+    stylix.targets.gdm.enable = lib.mkForce false;
+    stylix.targets.gtk.enable = true;
+    stylix.targets.qt.enable = true;
+
     polarity = "dark";
     opacity.terminal = 1.0;
     cursor = {
