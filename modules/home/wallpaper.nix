127e704d9d0d8b91cd9c06793b314b59ae1f8b6b
moons-14 <moons@moons14.com>
2025-10-29 20:13:35 +0900

    hash

--- modules/home/wallpaper.nix
+++ modules/home/wallpaper.nix
@@ -4,7 +4,7 @@ let
     owner = "orangci";
     repo  = "walls-catppuccin-mocha";
     rev   = "40912e6418737e93b59a38bcf189270cbf26656d";
-    hash  = "sha256-XXXXXXXXXXXX";   # nix-shell -p nix-prefetch-github --run 'nix-prefetch-github orangci walls-catppuccin-mocha --rev 40912e6418737e93b59a38bcf189270cbf26656d'
+    hash  = "sha256-1QR3J+wgLVNjucPSVELv0FicNt+jM2KPLU5ubBfxjHI=";   # nix-shell -p nix-prefetch-github --run 'nix-prefetch-github orangci walls-catppuccin-mocha --rev 40912e6418737e93b59a38bcf189270cbf26656d'
   };
 in {
   home.file."wallpapers" = {
